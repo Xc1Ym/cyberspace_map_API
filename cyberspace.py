2f2f483def5c38e9e69e62dadb47aea52fa117fe
xdym <xuedongyuming2233@gmail.com>
2021-07-26 16:46:31 +0800

    add re

--- cyberspace.py
+++ cyberspace.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     #print(search, szie, page)
     # 正则匹配以适应各API
     search_re.Re().regular(search)
+    '''
     if szie == -1 and page == -2:
         fofa_api.fofa().Rule()
     elif szie == -2 and page == -1:
@@ -20,7 +21,7 @@ if __name__ == '__main__':
     else:
         fofa_api.fofa().Get_search(fofa_email, fofa_key, search, szie, page)
         Zoom_eye.zoomeye_key()
-
+'''
     # 三个任选
     #fofa_api.fofa().Terminal(fofa_email,fofa_key) # 调用Fofa API
     #shodan_api # 调用shodan API
--- setting/search_re.py
+++ setting/search_re.py
@@ -3,6 +3,18 @@ import re
 class Re:
     @staticmethod
     def regular(search):
-        pattern = re.compile('((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}')
-        search_ip = pattern.search(search)
-        print(search_ip.group())
+        re_ip = re.compile('((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}')# 匹配IP
+        re_domain = re.compile('[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?')# 匹配域名
+
+        search_domain = re_domain.search(search)
+        if not search_domain is None:
+            search_ip = re_ip.search(search)
+            print(search_domain)
+            print(1)
+            if not search_ip is None:
+                print(search_ip)
+                print(2)
+            else:
+                print(3)
+        else:
+            print(4)
